515bb9027b20937ecf8d4f291a4ea2aae2660721
Rodion <rodionvitenberg@gmail.com>
2026-04-29 18:43:08 +0600

    2

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -4,4 +4,4 @@ from .models import Event
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ['id', 'title', 'description', 'description_en', 'image', 'event_date', 'created_at']
+        fields = ['id', 'title', 'title_en', 'description', 'description_en', 'image', 'event_date', 'created_at']
